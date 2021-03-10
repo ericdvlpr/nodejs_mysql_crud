bc941e013ed256d8c2ebc465c50afc239ffde730
ericdvlpr <ericjaucian@gmail.com>
2021-03-10 22:19:11 +0800

    crud function added

--- routes/people.js
+++ routes/people.js
@@ -12,4 +12,37 @@ Router.get("/", (req, res)=>{
     })
 })
 
+Router.get("/:id", (req, res)=>{
+    mysqlConnection.query("SELECT * FROM people WHERE id = ?", [req.params.id],(err, rows,fields)=>{
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+Router.post("/add", (req, res)=>{
+    const params = req.body;
+    mysqlConnection.query("INSERT INTO people SET ?", params ,(err, rows,fields)=>{
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+Router.put("/edit", (req, res)=>{
+    
+    const {id, name, address,contact} = req.body;
+
+    mysqlConnection.query("UPDATE people SET name = ?, address = ?, contact = ? WHERE id= ?", [name,address,contact,id] ,(err, rows,fields)=>{
+        if(!err){
+            res.send(rows);
+        }else{
+            console.log(err);
+        }
+    })
+})
 module.exports = Router;
